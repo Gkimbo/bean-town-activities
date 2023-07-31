1a970b583c322c3b706382ebff483648ce321e98
Gavin Kimball <you@example.com>
2023-07-31 15:48:50 -0400

    add models to Index.js

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -1,4 +1,6 @@
 // include all of your models here using CommonJS requires
-const User = require("./User.js")
+const User = require("./User.js");
+const Category = require("./Category.js");
+const Activity = require("./Activity.js");
 
-module.exports = {User};
+module.exports = { User, Category, Activity };
