cb351005d5b1d96cecfb3dd4003773cc4af56f82
Gavin Kimball <you@example.com>
2023-07-31 15:48:09 -0400

    add relation mappings to category model

--- server/src/models/Category.js
+++ server/src/models/Category.js
@@ -2,12 +2,26 @@ const Model = require("./Model.js");
 const uniqueFactory = require("objection-unique");
 
 const unique = uniqueFactory({
-    fields: ["name"]
+    fields: ["name"],
 });
 
 class Category extends unique(Model) {
     static get tableName() {
         return "categories";
+    }
+    static get relationMappings() {
+        const { Activity } = require("./index.js");
+
+        return {
+            activities: {
+                relation: Model.HasManyRelation,
+                modelClass: Activity,
+                join: {
+                    from: "categories.id",
+                    to: "activities.categoryId",
+                },
+            },
+        };
     }
 }
 
