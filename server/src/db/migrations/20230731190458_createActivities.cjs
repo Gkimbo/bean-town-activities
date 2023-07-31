6715d61d39d563fe56ecd8d059063a504d17736c
Gavin Kimball <you@example.com>
2023-07-31 15:32:59 -0400

    Formatting the Migrations

--- server/src/db/migrations/20230731190458_createActivities.cjs
+++ server/src/db/migrations/20230731190458_createActivities.cjs
@@ -6,20 +6,20 @@
  * @param {Knex} knex
  */
 exports.up = async (knex) => {
-    return knex.schema.createTable("activities" , (table) => {
-        table.bigIncrements("id")
-        table.string("name").notNullable()
-        table.bigInteger("categoryId").notNullable().unsigned().index().references("categories.id")
-        table.text("location").notNullable()
-        table.text("description")
-        table.timestamp("createdAt").notNullable().defaultTo(knex.fn.now())
-        table.timestamp("updatedAt").notNullable().defaultTo(knex.fn.now())
-    })
-}
+    return knex.schema.createTable("activities", (table) => {
+        table.bigIncrements("id");
+        table.string("name").notNullable();
+        table.bigInteger("categoryId").notNullable().unsigned().index().references("categories.id");
+        table.text("location").notNullable();
+        table.text("description");
+        table.timestamp("createdAt").notNullable().defaultTo(knex.fn.now());
+        table.timestamp("updatedAt").notNullable().defaultTo(knex.fn.now());
+    });
+};
 
 /**
  * @param {Knex} knex
  */
 exports.down = (knex) => {
-    return knex.schema.dropTableIfExists("activities")
-}
+    return knex.schema.dropTableIfExists("activities");
+};
--- server/src/db/migrations/20230731191518_createCategories.cjs
+++ server/src/db/migrations/20230731191518_createCategories.cjs
@@ -7,16 +7,16 @@
  */
 exports.up = async (knex) => {
     return knex.schema.createTable("categories", (table) => {
-        table.bigIncrements("id")
-        table.string("name")
-        table.timestamp("createdAt").notNullable().defaultTo(knex.fn.now())
-        table.timestamp("updatedAt").notNullable().defaultTo(knex.fn.now())
-    })
-}
+        table.bigIncrements("id");
+        table.string("name");
+        table.timestamp("createdAt").notNullable().defaultTo(knex.fn.now());
+        table.timestamp("updatedAt").notNullable().defaultTo(knex.fn.now());
+    });
+};
 
 /**
  * @param {Knex} knex
  */
 exports.down = (knex) => {
-    return knex.schema.dropTableIfExists("categories")
-}
+    return knex.schema.dropTableIfExists("categories");
+};
