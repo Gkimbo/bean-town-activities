98cdd7a79126d5d22cfdf69dad792600592f86cd
Gavin Kimball <you@example.com>
2023-07-31 16:32:49 -0400

    Finished index page

--- client/src/components/categoriesList.js
+++ client/src/components/categoriesList.js
@@ -1,10 +1,42 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 const categoriesList = (props) => {
+    const [categories, setCategories] = useState([])
+
+    const getCategories = async () => {
+        try {
+            const response = await fetch("/api/v1/categories")
+            if(!response.status){
+                const error = new Error(`${response.status} (${response.statusText})`)
+                throw error
+            }
+            const responseData = await response.json()
+            console.log(responseData.categories)
+            setCategories(responseData.categories)
+        } catch (error) {
+            console.error("Error in fetch!")
+            console.error(error.message)
+        }
+    }
+
+    useEffect(() => {
+        getCategories()
+    }, [])
+
+    const listOfCategories = categories.map(category => {
+        return(
+            <li key={category.id}>
+                {category.name}
+            </li>
+        )
+    })
 
     return (
         <div className="categories-list">
-            <h1>This is things to do list</h1>
+            <h1>Activities!</h1>
+            <ul>
+                {listOfCategories}
+            </ul>
         </div>
     )
 }
--- server/src/db/Seeder.js
+++ server/src/db/Seeder.js
@@ -1,9 +1,12 @@
 /* eslint-disable no-console */
 import { connection } from "../boot.js"
+import CategorySeeder from "./seeders/CategoriesSeeder.js"
 
 class Seeder {
   static async seed() {
     // include individual seed commands here
+    console.log("seeding Categories")
+    await CategorySeeder.seed()
 
     console.log("Done!")
     await connection.destroy()
