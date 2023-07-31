388af31f6536e4bbfeff2343b0f385e8b4795943
Gavin Kimball <you@example.com>
2023-07-31 13:09:32 -0400

    Add placesList component

---
+++ client/src/components/placesList.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const placesList = (props) => {
+
+    return (
+        <div className="places-list">
+
+            <h1>This is things to do list</h1>
+        </div>
+    )
+}
+
+export default placesList
