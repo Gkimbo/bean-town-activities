a4fe4b9e12b35f4f6e0cb3772f9a8092634382d3
Gavin Kimball <you@example.com>
2023-08-07 16:56:17 -0400

    Make delete button work

--- client/src/components/EditReviews.js
+++ client/src/components/EditReviews.js
@@ -2,58 +2,47 @@ import React, { useState, useEffect } from "react";
 
 const EditReviews = (props) => {
   const [reviewsToEdit, setReviewsToEdit] = useState([]);
-  console.log(reviewsToEdit)
-    const getReviews = async () =>{
-        try {
-            const response = await fetch(`/api/v1/activities/${props.match.params.id}`)
-            if(!response){
-                const error = new Error(`${response.status} (${response.statusText})`)
-                throw error
-            }
-            const responseData = await response.json()
-            setReviewsToEdit(responseData.activity.reviews)
-        } catch (error) {
-            console.error("Error in fetch!", error.message)
-        }
+
+  const getReviews = async () => {
+    try {
+      const response = await fetch(`/api/v1/activities/${props.match.params.id}`);
+      if (!response.ok) {
+        throw new Error(`${response.status} (${response.statusText})`);
+      }
+      const responseData = await response.json();
+      setReviewsToEdit(responseData.activity.reviews);
+    } catch (error) {
+      console.error("Error in fetch!", error.message);
     }
+  };
 
-    const removeReview = async (id) => {
-        try {
-            const response = await fetch(`/api/v1/activities/${props.match.params.id}/reviews/${id}`,
-            {method: 'DELETE'})
-
-            if(!response){
-                const error = new Error(`${response.status} (${response.statusText})`)
-                throw error
-            }
-            setReviewsToEdit(reviewsToEdit.filter(review => review.id !== id))
-        } catch (error) {
-            console.error("Error in fetch!", error.message)
-        }
+  const removeReview = async (id) => {
+    try {
+      const response = await fetch(`/api/v1/activities/${props.match.params.id}/reviews/${id}`, {
+        method: "DELETE",
+      });
+      if (!response.ok) {
+        throw new Error(`${response.status} (${response.statusText})`);
+      }
+      setReviewsToEdit(reviewsToEdit.filter((review) => review.id !== id));
+    } catch (error) {
+      console.error("Error in fetch!", error.message);
     }
+  };
 
-    useEffect(()=> {
-        getReviews()
-    }, [])
+  useEffect(() => {
+    getReviews();
+  }, []);
 
-    const handleDelete = (event) => {
-        event.preventDefault()
-        console.log(event)
-        removeReview(event.target.id)
-    }
+  const handleDelete = (id) => {
+    removeReview(id);
+  };
 
-    const listOfReviews = reviewsToEdit.map(({id, content}) => {
+  const listOfReviews = reviewsToEdit.map(({ id, content }) => {
     return (
       <li key={id}>
         {content}
-        <button class="noselect" onClick={handleDelete}>
-          <span class="text">Delete</span>
-          <span class="icon">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-              <path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"></path>
-            </svg>
-          </span>
-        </button>
+        <div onClick={() => handleDelete(id)}>Delete</div>
       </li>
     );
   });
--- server/src/routes/api/v1/reviewRouter.js
+++ server/src/routes/api/v1/reviewRouter.js
@@ -1,16 +1,15 @@
 import express from "express"
 import { Review } from "../../../models/index.js"
 
-const reviewsRouter = new express.Router({mergeParams: true})
+const reviewsRouter = new express.Router({ mergeParams: true })
 
 reviewsRouter.delete("/:id", async (req, res) => {
-    try{
-        const review = await Review.query().findById(req.params.id)
-        
-        await review.$query().delete()
-        return res.status(200).json({review})
-    }catch(err){
-        return res.status(500).json({errors:err})
+    try {
+        const reviewToDelete = await Review.query().findById(req.params.id)
+        await reviewToDelete.$query().delete()
+        return res.status(200).json({ review: reviewToDelete })
+    } catch (err) {
+        return res.status(500).json({ errors: err })
     }
 })
 
